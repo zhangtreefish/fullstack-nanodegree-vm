144a9e8edeabb28e6c891b2f85c37214292add36
zhangtreefish <zhangtreefish@users.noreply.github.com>
2016-01-10 23:00:11 -0600

    chore:add more rows to database

--- vagrant/manyRestaurants.py
+++ vagrant/manyRestaurants.py
@@ -10,9 +10,11 @@ session = DBSession()
 myFirstRestaurant = Restaurant(name='steam', description='all things steamed')
 mySecondRestaurant = Restaurant(name='3Fs',description='that features fiber, fitness, and fluid')
 myThirdRestaurant = Restaurant(name='Eden',description='out of your dream garden')
+myFourthRestaurant = Restaurant(name='School Lunch', description='balanced lunch for the precious bodies and minds')
 session.add(myFirstRestaurant)
 session.add(mySecondRestaurant)
 session.add(myThirdRestaurant)
+session.add(myFourthRestaurant)
 session.commit()
 restaurant_list=session.query(Restaurant).all()
 print session.query(Restaurant).first().name
@@ -23,34 +25,69 @@ myFirstMenu = MenuItem(name='jade',
 	                   price='$2.50',
 	                   course='vegetable',
                        restaurant=myFirstRestaurant)
-mySecondMenu = MenuItem(name='FragrantSnow',
+mySecondMenu = MenuItem(name='fragrant snow',
                         description='sweet rice flour balls encasing honey-infused sweet olive blossoms and coconut oil',
                         price='$4.00',
                         course='dessert',
                         restaurant=myFirstRestaurant)
+myThirdMenu = MenuItem(name='Four-layered dip',
+                       description='organic black beans, avacado, tomato, cheese, dressed in lemon juice, srved with tortilla chips',
+                       price='$4.00',
+                       course='One Complete Meal',
+                       restaurant=mySecondRestaurant)
+myFourthMenu = MenuItem(name='chicken noodle soup',
+                       description='what else can you say? made with carrots, celery, onion,garlic, tomato, zucchini, and ginger root',
+                       price='$5.00',
+                       course='One Complete Meal',
+                       restaurant=myFourthRestaurant)
+myFifthMenu = MenuItem(name='ocean',
+                       description='soup made of tilapia,  celery, cilantro, green onion,garlic, tomato, zucchini, and ginger root',
+                       price='$5.00',
+                       course='One Complete Meal',
+                       restaurant=myFourthRestaurant)
+mySixthMenu = MenuItem(name='seaweed',
+                       description='Wakame salad in green onion, hemp heart,sesame oil, and salt,served with two pieces of baked tofu, and a bun',
+                       price='$5.00',
+                       course='One Complete Meal',
+                       restaurant=mySecondRestaurant)
+mySeventhMenu = MenuItem(name='ocean',
+                       description='soup made of tilapia,  celery, cilantro, green onion,garlic, tomato, zucchini, and ginger root',
+                       price='$5.00',
+                       course='One Complete Meal',
+                       restaurant=myFourthRestaurant)
 session.add(myFirstMenu)
 session.add(mySecondMenu)
+session.add(myThirdMenu,myFourthMenu)
+# session.add(myFourthMenu)
+session.add(myFifthMenu)
+session.add(mySixthMenu)
+session.add(mySeventhMenu)
 session.commit()
 laRes = session.query(MenuItem).first().name
 print laRes
 
 myFirstCondition = Condition(name='diabetes',signs_and_symptoms='thirst, fatigue, frequent urination, weight loss')
+mySecondCondition = Condition(name='gray hair',signs_and_symptoms='natural graying of hair')
+session.add(mySecondCondition)
+session.commit()
 # link a menu to myFirstCondition
 diabeticMenu1 = MenuItem(name='baked sweet potato',
                         description='sweet potato baked at 350 for 45 minutes, with skin',
                         price='$3.00',
                         course='vegetable',
-                        restaurant=mySecondRestaurant)
+                        restaurant=myFirstRestaurant)
 session.add(diabeticMenu1)
 session.commit()
 myFirstCondition.suggested_menus.append(diabeticMenu1)
 session.add(myFirstCondition)
 session.commit()
 laCon = session.query(Condition).all()
-print laCon[0].suggested_menus[0].name
+print laCon[1].suggested_menus[1].name
 
 myFirstUser = User(name='treefish',email='zhangtreefish@yahoo.com', picture='')
-session.add(myFirstUser)
+mySecondUser = User(name='bob',email='fearlessluke8@gmail.com', picture='')
+
+session.add(mySecondUser)
 session.commit()
 myFirstUser.restaurants.append(myFirstRestaurant)
 myFirstUser.conditions.append(myFirstCondition)
